a739eb3920cd410adf66a908a17723722763e8f6
Denjj <25715300+Denjj@users.noreply.github.com>
2021-03-23 00:15:14 -0700

    Add database functions to api

--- facefinder-api/server.js
+++ facefinder-api/server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : '',
+    password : '',
+    database : 'facefinder'
+  }
+});
 
 const app = express();
 app.use(
@@ -8,73 +19,77 @@ app.use(
   cors()
 );
 
-const testDatabase = {
-  users: [
-    {
-      id: '123',
-      username: 'John',
-      password: 'apple',
-      entries: 0,
-      joined: new Date()
-    },
-    {
-      id: '234',
-      username: 'Sara',
-      password: 'banana',
-      entries: 0,
-      joined: new Date()
-    }
-  ]
-}
-
-
 app.get('/', (req, res) => {
-  return res.send(testDatabase.users);
+  return res.send('success');
 })
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  testDatabase.users.forEach(user => {
-    if (user.id === id) {
-      return res.json(user);
-    }
-  });
-  return res.status(400).json('No such user exists');
+  db.select('*').from('users').where({ id })
+    .then(user => {
+      if (user.length){
+        res.json(user[0]);
+      } else {
+        res.status(400).json('Not found');
+      }
+    })
+    .catch(err => res.status(400).json('error getting user'))
+
 })
 
 app.post('/signin', (req, res) => {
-  if (req.body.username === testDatabase.users[0].username &&
-      req.body.password === testDatabase.users[0].password){
-        return res.json(testDatabase.users[0]);
+  db.select('username', 'hash').from('login')
+    .where('username', '=', req.body.username)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        db.select('*').from('users')
+          .where('username', '=', req.body.username)
+          .then(user => {
+            res.json(user[0])
+          })
+          .catch(err => res.status(400).json('unable to get user'))
+      } else {
+        res.status(400).json('wrong credentials');
       }
-  return res.status(400).json('error logging in');
+    })
 })
 
 app.post('/register', (req, res) => {
   const { username, password } = req.body;
-  testDatabase.users.push({
-    id: '345',
-    username: username,
-    password: password,
-    entries: 0,
-    joined: new Date()
-  });
-  return res.json(testDatabase.users[testDatabase.users.length-1]);
+  const hash = bcrypt.hashSync(password);
+  db.transaction(trx => {
+    trx.insert({
+      hash: hash,
+      username: username
+    })
+    .into('login')
+    .returning('username')
+    .then(loginUsername => {
+      return trx('users')
+      .returning('*')
+      .insert({
+        username: loginUsername[0],
+        joined: new Date()
+      })
+      .then(user => {
+        res.json(user[0]);
+      })
+    })
+    .then(trx.commit)
+    .catch(trx.rollback)
+  })
+  .catch(err => res.status(400).json('Unable to register'))
+  
 })
 
 app.put('/image', (req, res) => {
   const { id } = req.body;
-  let found = false;
-  testDatabase.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  });
-  if (!found) {
-    return res.status(400).json('No such user exists');
-  }
+  db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {res.json(entries[0])})
+    .catch(err => res.status(400).json('unable to get entries'));
 })
 
 app.listen(3000, () => {
