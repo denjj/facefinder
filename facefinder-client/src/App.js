71694f932fcdbd95de2b9f52d82c899f556c2a5d
Denjj <25715300+Denjj@users.noreply.github.com>
2021-04-01 02:14:46 -0700

    Add initalstate for clearing state on signout

--- facefinder-client/src/App.js
+++ facefinder-client/src/App.js
@@ -125,22 +125,24 @@ const app = new Clarifai.App({
   apiKey: '5bf6e76d0e974dc0b06a41f66805fdb2'
  });
 
+const initialState = {
+    input: '',
+    imageUrl: '',
+    box: {},
+    route: 'signin',
+    isSignedIn: false,
+    user: {
+      id: '',
+      username: '',
+      entries: 0,
+      joined: ''
+    }
+}
+
 class App extends Component {
   constructor(){
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        username: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = initialState;
   }
 
   loadUser = (data) => {
@@ -208,7 +210,7 @@ class App extends Component {
       this.setState({isSignedIn: true});
     } else {
       //console.log("currently signed out")
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     }
   }
 
